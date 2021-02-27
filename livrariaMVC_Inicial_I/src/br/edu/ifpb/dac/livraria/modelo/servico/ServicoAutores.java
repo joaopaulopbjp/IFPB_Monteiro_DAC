e18d59dd1c6928f3617e982ba73389c48907b5e9
jp <jp@jpls-Inspiron-5480>
2021-02-27 06:43:58 -0300

    inclusao

--- livrariaMVC_Inicial_I/src/br/edu/ifpb/dac/livraria/modelo/servico/ServicoAutores.java
+++ livrariaMVC_Inicial_I/src/br/edu/ifpb/dac/livraria/modelo/servico/ServicoAutores.java
@@ -31,15 +31,39 @@ public class ServicoAutores{
 	    EntityManager manager = new JPAUtils().getEntityManager();
 
 	    //TODO Realizar uma consulta usando JPA para retornar uma lista com todos os autores do banco
-	    
-		return null;
+		return manager.createQuery("select a from Autor a",Autor.class).getResultList();
 	}
 
 	public Autor buscaPelaId(Integer autorId) {
 	    EntityManager manager = new JPAUtils().getEntityManager();
 
 	    //TODO Realizar uma consulta usando JPA para buscar o autor pelo id
-				
-		return null;
+		Autor autor = manager.find(Autor.class,autorId);
+		
+		return autor;
+	}
+	
+	public void altera(Autor autor) {
+		System.out.println("[INFO] Alterando o Autor " + autor.getNome());
+	    EntityManager manager = new JPAUtils().getEntityManager();
+	    manager.getTransaction().begin();
+	    manager.merge(autor);
+	    manager.getTransaction().commit();
+	    System.out.println("[INFO] Autor " + autor.getNome()+" alterado com sucesso.");	
+	}
+
+	public void remove(int id) {
+		System.out.println("[INFO] Removendo o Autor com id: " + id);
+		
+		Autor autor = new Autor();
+		autor.setId(id);
+		
+	    EntityManager manager = new JPAUtils().getEntityManager();
+	    manager.getTransaction().begin();
+	    autor = manager.merge(autor);
+	    manager.remove(autor);
+	    manager.getTransaction().commit();
+	    System.out.println("[INFO] Autor com id: " + id+" removido com sucesso.");
+		
 	}
 }	
